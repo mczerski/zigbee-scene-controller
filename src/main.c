9b8aa23c53c27434b6ba2516b0daca5da03a3c86
Marek Czerski <m.czerski@ap-tech.pl>
2025-05-04 19:10:08 +0200

    use adc power supply voltage reading

--- src/main.c
+++ src/main.c
@@ -10,6 +10,7 @@
 
 #include <zephyr/kernel.h>
 #include <zephyr/logging/log.h>
+#include <zephyr/drivers/adc.h>
 #include <dk_buttons_and_leds.h>
 #include <ram_pwrdn.h>
 
@@ -41,7 +42,19 @@
 #error Define ZB_ED_ROLE to compile light switch (End Device) source code.
 #endif
 
+#if !DT_NODE_EXISTS(DT_PATH(zephyr_user)) || \
+    !DT_NODE_HAS_PROP(DT_PATH(zephyr_user), io_channels)
+#error "No suitable devicetree overlay specified"
+#endif
+
 LOG_MODULE_REGISTER(app, LOG_LEVEL_INF);
+
+#define DT_SPEC_AND_COMMA(node_id, prop, idx) ADC_DT_SPEC_GET_BY_IDX(node_id, idx),
+
+/* Data of ADC io-channels specified in devicetree. */
+static const struct adc_dt_spec adc_channels[] = {
+    DT_FOREACH_PROP_ELEM(DT_PATH(zephyr_user), io_channels, DT_SPEC_AND_COMMA)
+};
 
 struct buttons_context {
     int state;
@@ -245,6 +258,24 @@ static void configure_timers(void)
     k_timer_init(&buttons_ctx.press_timer, button_timer, NULL);
 }
 
+static void configure_adc(void)
+{
+    int err;
+    /* Configure channels individually prior to sampling. */
+    for (size_t i = 0U; i < ARRAY_SIZE(adc_channels); i++) {
+        if (!adc_is_ready_dt(&adc_channels[i])) {
+            LOG_ERR("ADC controller device %s not ready\n", adc_channels[i].dev->name);
+            return;
+        }
+
+        err = adc_channel_setup_dt(&adc_channels[i]);
+        if (err < 0) {
+            LOG_ERR("Could not setup channel #%d (%d)\n", i, err);
+            return;
+        }
+    }
+}
+
 /**@brief Function to toggle the identify LED.
  *
  * @param  bufid  Unused parameter, required by ZBOSS scheduler API.
@@ -351,6 +382,7 @@ int main(void)
     /* Initialize. */
     configure_gpio();
     configure_timers();
+    configure_adc();
     register_factory_reset_button(FACTORY_RESET_BUTTON);
 
     zigbee_erase_persistent_storage(ERASE_PERSISTENT_CONFIG);
@@ -385,6 +417,26 @@ int main(void)
 
     LOG_INF("Zigbee R23 Light Switch example started");
 
+    uint16_t buf;
+    struct adc_sequence sequence = {
+        .buffer = &buf,
+        /* buffer size in bytes, not number of samples */
+        .buffer_size = sizeof(buf),
+    };
+    adc_sequence_init_dt(&adc_channels[0], &sequence);
+    int err = adc_read_dt(&adc_channels[0], &sequence);
+    if (err < 0) {
+        LOG_ERR("Could not read (%d)\n", err);
+    }
+    int32_t val_mv = 5 * (int32_t)buf;
+    LOG_INF("raw: %d", val_mv);
+    err = adc_raw_to_millivolts_dt(&adc_channels[0], &val_mv);
+    /* conversion to mV may not be supported, skip if not */
+    if (err < 0) {
+        LOG_ERR("value in mV not available\n");
+    } else {
+        LOG_INF("voltage: %d mV", val_mv);
+    }
     //zb_uint8_t battery_attribute = 32;
     //if (zb_zcl_set_attr_val(
     //        MY_DEVICE_ENDPOINT,
