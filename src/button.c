3afc834ff3081463b6bea3774c449ddc613422a4
Marek Czerski <m.czerski@ap-tech.pl>
2025-05-18 15:59:11 +0200

    reboot to dfu on key combination + add continous press events

--- src/button.c
+++ src/button.c
@@ -1,9 +1,18 @@
 #include <zephyr/logging/log.h>
 #include <zephyr/input/input.h>
+#include <zboss_api.h>
 #include "zigbee.h"
 
 
+#define LONG_PRESS_INTERVAL 1000
+
 LOG_MODULE_REGISTER(button, LOG_LEVEL_INF);
+
+static void continous_press_timer(zb_uint8_t scene_id)
+{
+    ZB_SCHEDULE_APP_ALARM(continous_press_timer, scene_id, ZB_MILLISECONDS_TO_BEACON_INTERVAL(LONG_PRESS_INTERVAL));
+    send_scene(scene_id);
+}
 
 static void button_handler(struct input_event *evt, void *user_data)
 {
@@ -21,6 +30,17 @@ static void button_handler(struct input_event *evt, void *user_data)
         (scene_type == 4 && evt->value == 0) // double press
     ) {
         scene_id = evt->code;
+        if (scene_type == 2 && evt->value == 1) {
+            ZB_SCHEDULE_APP_ALARM(
+                continous_press_timer,
+                (5 << 4) | (scene_id & 0xF),
+                ZB_MILLISECONDS_TO_BEACON_INTERVAL(LONG_PRESS_INTERVAL)
+            );
+        }
+    }
+    else if (scene_type == 2 && evt->value == 0) // long press deactivation
+    {
+        ZB_SCHEDULE_APP_ALARM_CANCEL(continous_press_timer, ZB_ALARM_ANY_PARAM);
     }
 
     send_scene(scene_id);
--- src/secret_buttons.c
+++ src/secret_buttons.c
@@ -1,12 +1,21 @@
+#include <zephyr/logging/log.h>
 #include <zephyr/input/input.h>
 #include <zephyr/sys/reboot.h>
+#include <zephyr/drivers/retained_mem.h>
 #include <zboss_api.h>
 #include <ram_pwrdn.h>
+
+
+LOG_MODULE_REGISTER(system, LOG_LEVEL_INF);
+
+const static struct device *gpregret_device = DEVICE_DT_GET(DT_NODELABEL(gpregret1));
 
 static void secret_button_handler(struct input_event *evt, void *user_data)
 {
     static bool first_reset_pressed = false;
     static bool second_reset_pressed = false;
+    static bool first_dfu_pressed = false;
+    static bool second_dfu_pressed = false;
     if (evt->type == INPUT_EV_KEY && evt->code == 0xF3) {
         first_reset_pressed = evt->value;
     }
@@ -14,12 +23,24 @@ static void secret_button_handler(struct input_event *evt, void *user_data)
         second_reset_pressed = evt->value;
     }
     if (first_reset_pressed && second_reset_pressed) {
+        LOG_INF("Reseting network configuration");
         ZB_SCHEDULE_APP_CALLBACK(zb_bdb_reset_via_local_action, 0);
         return;
     }
-    if (evt->type == INPUT_EV_KEY && evt->code == 0xF1 && evt->value == 1) {
-        // TODO: use zephyr driver
-        NRF_POWER->GPREGRET = 0x57;
+    if (evt->type == INPUT_EV_KEY && evt->code == 0xF1) {
+        first_dfu_pressed = evt->value;
+    }
+    if (evt->type == INPUT_EV_KEY && evt->code == 0xF2) {
+        second_dfu_pressed = evt->value;
+    }
+    if (first_dfu_pressed && second_dfu_pressed) {
+        LOG_INF("Rebooting into DFU");
+        uint8_t reboot_to_dfu = 0x57;
+        int err = retained_mem_write(gpregret_device, 0, &reboot_to_dfu, sizeof(reboot_to_dfu));
+        if (err != 0) {
+            LOG_ERR("Could not write gpregret (%d)", err);
+            return;
+        }
         power_up_unused_ram();
         sys_reboot(SYS_REBOOT_COLD);
         return;
